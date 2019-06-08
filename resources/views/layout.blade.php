b866b1e0fae4e44a56bee82819b1c6b998a96cdc
actionhanky <samhields@Sams-MacBook-Pro-6.local>
2019-06-08 22:32:12 +0100

    blade changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>homepage</title>
+    <title>@yield('title','???')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
@@ -15,6 +15,7 @@
     <ul>
         <li><a href="/contact">Contact</a> us to find out more!</li>
         <li><a href="/about">About us</a></li>
+        <li><a href="/">Home</a></li>
     </ul>
 </body>
 </html>
