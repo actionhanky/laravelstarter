8aa1339057c8d5152d2c3d7deea18970e40d960c
actionhanky <samhields@Sams-MacBook-Pro-6.local>
2019-06-08 18:40:45 +0100

    extra blades

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>homepage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+    @yield('content')
+
+    <ul>
+        <li><a href="/contact">Contact</a> us to find out more!</li>
+        <li><a href="/about">About us</a></li>
+    </ul>
+</body>
+</html>
